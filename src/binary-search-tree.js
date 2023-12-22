caf4ac2785f77ca32b9fa77c3f473104972de329
Elizaveta Zaharova <LizaZaharova99@mail.ru>
2023-12-22 14:59:32 +0300

    init: stark BST task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,32 +1,88 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.head = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        if ((data === null)&(data === undefined)) {
+          return;
+        }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        this.head = addNewNode(this.head, data);
+
+
+        function addNewNode(currentNode, new_value) {
+          // if not exist:
+            if (!currentNode) {
+              return new Node(new_value);
+            }
+
+            if (new_value < currentNode.data) {
+                currentNode.left = addNewNode(currentNode.left, new_value)
+                } else if (new_value > currentNode.data) {
+                  currentNode.right = addNewNode(currentNode.right, new_value)
+                }
+                return currentNode;        
+          }    
+        }
+    
+
+  has(data) {
+
+      return existNode(this.head, data);
+
+      function existNode(currentNode, find_data) {
+        if (!currentNode) {
+          return false;
+        }
+
+
+        if (currentNode.data === find_data) {
+          return true;
+        }
+
+        else if (currentNode.data > find_data) {
+          return existNode(currentNode.left, find_data)
+          } else {
+          return existNode(currentNode.right, find_data)
+        }
+    }
+    }
+  
+
+  find(data) {
+    return findNode(this.head, data);
+
+      function findNode(currentNode, find_data) {
+        if (!currentNode) {
+          return null;
+        }
+
+
+        if (currentNode.data === find_data) {
+          return currentNode;
+        }
+
+        else if (currentNode.data > find_data) {
+          return findNode(currentNode.left, find_data)
+          } else {
+          return findNode(currentNode.right, find_data)
+        }
+    }
+}
 
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
